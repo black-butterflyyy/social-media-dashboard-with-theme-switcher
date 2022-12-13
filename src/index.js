868127253cedc1085ec6f6168fd5a9abb1e3ea9b
Hosna <blackbutterfllly@gmail.com>
2022-12-13 18:51:28 +0330

    Finish dark theme style for mobile design

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-﻿
+﻿const checkboxInput = document.getElementById("dark-mode");
+
+function changeTheme() {
+  const bodyElement = document.querySelector("body");
+  bodyElement.classList.toggle("dark-theme");
+}
+
+checkboxInput.addEventListener("change", changeTheme);
